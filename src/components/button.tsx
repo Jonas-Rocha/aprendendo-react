72303b3c3161473d07c9db25d380b582c8e7ac7e
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-08-29 09:36:02 -0300

    Rest Operator para trazer todas as props nativas de um elemento HTML

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -16,6 +16,15 @@ export function Button(props: Props) {
 
 //Passando Métodos no Componente
 
-export function Button({ name, onClick }: Props) {
-  return <button onClick={onClick}>{name}</button>
+export function Button({ name, ...rest }: Props) {
+  //Rest Operator
+
+  /*
+  Nesta aula, mostrei uma abordagem diferente para lidar com propriedades em um componente. 
+  Em vez de passar o onClick explicitamente, utilizei o operador rest (...) para pegar todas as propriedades restantes de uma vez.
+  Isso simplifica o código e garante que todas as funcionalidades do botão sejam mantidas. 
+  Ao clicar em "criar", tudo funciona normalmente, pois estamos repassando todas as propriedades de forma eficiente. 
+  Essa técnica é bem útil para otimizar nosso trabalho com componentes!
+  */
+  return <button {...rest}>{name}</button>
 }
