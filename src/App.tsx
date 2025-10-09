0309b4d307f4082f774e564d7f40946aca0f41af
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-10-09 13:57:54 -0300

    Exemplificando a importancia do hook useState no DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import "./global.css"
-import { Button } from "./components/button"
-import styles from "./app.module.css"
-import { useMessage } from "./hooks/useMessage"
-
-
-
-
-
+import "./global.css";
+import { useState } from "react";
+import { Button } from "./components/button";
+import styles from "./app.module.css";
+// import { useMessage } from "./hooks/useMessage"
 
 export function App() {
-    const message = useMessage({name: "Jonas", age: 27})
+  const [count /*estado*/, setCount /*função que atualiza o estado*/] =
+    useState(0);
 
-    return (
+  // const { show } = useMessage({name: "Jonas", age: 27})
+  // const { show } = useMessage()
 
-            <div className={styles.container}>
-                <Button name="Adicionar" onClick={() => message.show()}/>
-                <span>0</span>
-                <Button name="Remover"/>
-            </div>
-
-    )
+  return (
+    <div className={styles.container}>
+      {/* /* isso não acontece com uma função comum pois o react precisa verificar a mudança(estado) no componente para poder re-renderizar o DOM virtual na tela do usuário. por isso preciso do hook "useState", ele verifica isso para o react.*/}
+      <Button
+        name="Adicionar"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      />
+      <span>{count}</span>
+      {/* isso não acontece com uma função comum pois o react precisa verificar a mudança(estado) no componente para poder re-renderizar o DOM virtual na tela do usuário. por isso preciso do hook "useState", ele verifica isso para o react.*/}
+      <Button
+        name="Remover"
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      />
+    </div>
+  );
 }
--- src/hooks/useMessage.ts
+++ src/hooks/useMessage.ts
@@ -1,13 +1,13 @@
-type Props = {
-    name: string,
-    age: number
-}
+// type Props = {
+//     name: string,
+//     age: number
+// }
 
 
 
-export function useMessage({ name, age } : Props) {
-    function show() {
-        console.log(name, age, "Mensagem do meu próprio Hook!")
+export function useMessage(/*{ name, age } : Props*/) {
+    function show(message: string) {
+        console.log(message)
     }
 
     return { show }
